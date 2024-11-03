0d4682ae83a932fab77861dddb065af717cc3974
lantel-wm <zyzh@smail.nju.edu.cn>
2024-11-03 23:53:14 +0800

    add check_error

--- src/server.cpp
+++ src/server.cpp
@@ -1,9 +1,50 @@
+#include <cerrno>
 #include <cstddef>
 #include <cstdio>
+#include <cstring>
 #include <fmt/format.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+
+int check_error(const char* msg, int res) {
+    if (res == -1) {
+        fmt::print("{}: {}\n", msg, strerror(errno));
+        throw;
+    }
+    return res;
+}
+
+struct socket_address_fatptr {
+    struct sockaddr* m_addr;
+    socklen_t m_addrlen;
+};
+
+struct address_resolved_entry {
+    struct addrinfo* m_curr = nullptr;
+
+    socket_address_fatptr get_address() const {
+        return {m_curr->ai_addr, m_curr->ai_addrlen};
+    }
+
+    int create_socket() const {
+        int sockfd = socket(m_curr->ai_family, m_curr->ai_socktype, m_curr->ai_protocol);
+        if (sockfd == -1) {
+            fmt::print("socket: {}", strerror(errno));
+            throw;
+        }
+        return sockfd;
+    }
+
+    [[nodiscard]] bool next_entry() {
+        m_curr = m_curr->ai_next;
+        if (m_curr == nullptr) {
+            return false;
+        }
+        return true;
+    }
+
+};
 
 struct address_resolver {
     struct addrinfo* m_head = nullptr;
@@ -11,28 +52,35 @@ struct address_resolver {
     void resolve(const std::string& name, const std::string& service) {
         int err = getaddrinfo(name.c_str(), service.c_str(), NULL, &m_head);
         if (err != 0) {
-            
+            fmt::print("getaddrinfo: {} {}", gai_strerror(err), err);
+            throw;
+        }
+    }
+
+    address_resolved_entry get_first_entry() {
+        return {m_head};
+    }
+
+    address_resolver() = default;
+
+    address_resolver(address_resolver&& that) : m_head(that.m_head) {
+        that.m_head = nullptr;
+    }
+
+    ~address_resolver() {
+        if (m_head) {
+            freeaddrinfo(m_head);
         }
     }
 };
 
 int main() {
-    struct addrinfo *addrinfo;
-    int err = getaddrinfo("127.0.0.1", "80", NULL, &addrinfo);
-    if (err != 0) {
-        fmt::print("getaddrinfo: {}\n", gai_strerror(err));
-        return 1;
-    }
-
-    for (struct addrinfo* current = addrinfo; current != NULL; current = current->ai_next) {
-        fmt::print("family  : {}\n", addrinfo->ai_family);
-        fmt::print("socktype: {}\n", addrinfo->ai_socktype);
-        fmt::print("protocol: {}\n", addrinfo->ai_protocol);
-    }
-
-    int sockfd = socket(-1, addrinfo->ai_socktype, addrinfo->ai_protocol);
-    if (sockfd == -1) {
-        fmt::print("socket: {}\n", strerror(errno));
-        return 1;
-    }
+    address_resolver resolver;
+    resolver.resolve("127.0.0.1", "8080");
+    auto entry = resolver.get_first_entry();
+    int sockfd = entry.create_socket();
+    socket_address_fatptr addr = entry.get_address();
+    check_error("bind", bind(sockfd, addr.m_addr, addr.m_addrlen));
+    check_error("listen", listen(sockfd, SOMAXCONN));
+    
 }
