65a59a3ea872be5eb11aacf9b692ab9732bb1add
lantel-wm <zyzh@smail.nju.edu.cn>
2024-11-05 00:08:22 +0800

    add http_parser_base

--- src/server.cpp
+++ src/server.cpp
@@ -1,10 +1,11 @@
-#include <algorithm>
+#include <cassert>
 #include <cerrno>
 #include <cstddef>
 #include <cstdio>
 #include <cstring>
 #include <fmt/format.h>
 #include <map>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 #include <sys/types.h>
@@ -107,56 +108,91 @@ struct address_resolver {
     }
 };
 
-using StringMap = std::map<std::string, std::string>;
+using string_map = std::map<std::string, std::string>;
 
 struct http11_header_parser {
     std::string m_header;        // "GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: curl/7.81.0\r\nAccept: */*"
-    std::string m_heading_line;  // "GET / HTTP/1.1"
-    StringMap m_header_keys;     // {"Host": "127.0.0.1:8080", "Accept": "*/*", ...}
+    std::string m_headline;  // "GET / HTTP/1.1"
+    string_map m_header_keys;     // {"Host": "127.0.0.1:8080", "Accept": "*/*", ...}
     std::string m_body;          // The over-reading body (if exists)
     size_t content_length;
     bool m_header_finished;
     bool m_body_finished;
 
     void _parse_header() {
-        size_t pos = m_header.find("\r\n");
+        std::string_view header = m_header;
+        size_t pos = header.find("\r\n", 0, 2);
+        m_headline = std::string(header.substr(0, pos));
         while (pos != std::string::npos) {
             pos += 2; // skip "\r\n"
-            size_t next_pos = m_header.find("\r\n", pos);
+            size_t next_pos = m_header.find("\r\n", pos, 2);
             size_t line_len = std::string::npos;
             if (next_pos != std::string::npos) {
                 line_len = next_pos - pos;
             }
-            std::string line = m_header.substr(pos, line_len);
-            size_t colon = line.find(": ");
+            std::string_view line = header.substr(pos, line_len);
+            size_t colon = line.find(": ", 0, 2);
             if (colon != std::string::npos) {
-                std::string key = line.substr(0, colon);
-                std::string value = line.substr(colon + 2);
-                std::transform(key.begin(), key.end(), key.begin(), [] (char c) {
+                std::string key = std::string(line.substr(0, colon));
+                std::string_view value = line.substr(colon + 2);
+                // transform to lower case
+                for (char& c: key) {
                     if ('A' <= c && c <= 'Z')
                         c += 'a' - 'A';
-                    return c;
-                });
-                if (key == "content-length") {
-                    content_length = std::stoi(value);
                 }
+                m_header_keys.insert_or_assign(std::move(key), value);
             }
             pos = next_pos;
         }
     }
 
     void push_chunk(std::string_view chunk) {
-
+        assert(!m_header_finished);
+        size_t old_size = m_header.size();
+        m_header.append(chunk);
+        std::string_view header = m_header;
+        if (old_size < 4) {
+            old_size = 4;
+        }
+        old_size -= 4; // strip "\r\n\r\n"
+        size_t header_len = header.find("\r\n\r\n", old_size, 4);
+        if (header_len != std::string::npos) {
+            m_header_finished = true;
+            m_body = m_header.substr(header_len + 4);
+            m_header.resize(header_len);
+            _parse_header();
+        }
+        
     }
 
     [[nodiscard]] bool header_finished() const {
         return m_header_finished;
     }
+
+    std::string& headline() {
+        return m_headline;
+    }
+
+    std::string& headers_raw() {
+        return m_header;
+    }
+
+    string_map& headers() {
+        return m_header_keys;
+    }
+
+    int status() {
+
+    }
+
+    std::string& extra_body() {
+        return m_body;
+    }
 };
 
 
 template <typename HeaderParser = http11_header_parser>
-struct http_request_parser {
+struct _http_parser_base {
     HeaderParser m_header_parser;
     std::string m_header;
     std::string m_body;
@@ -164,18 +200,127 @@ struct http_request_parser {
     bool m_header_finished;
     bool m_body_finished;
 
-    [[nodiscard]] bool need_more_chunks() const {
-        return !m_body_finished;
+    [[nodiscard]] bool body_finished() const {
+        return m_header_parser.header_finished();
+    }
+
+    [[nodiscard]] bool request_finished() const {
+        return m_body_finished;
+    }
+
+    std::string& headers_raw() {
+        return m_header_parser.headers_raw();
+    }
+
+    string_map& headers() {
+        return m_header_parser.headers();
+    }
+
+    std::string headline() {
+        return m_header_parser.headline();
+    }
+
+    std::string _headline_first() {
+        // headline (request): "GET / HTTP/1.1"
+        // headline (response): "HTTP/1.1 200 OK"
+        std::string& line = headline();
+        size_t space = line.find(" ");
+        if (space == std::string::npos) {
+            return "";
+        }
+        return line.substr(0, space);
+    }
+
+    std::string _headline_second() {
+        // headline (request): "GET / HTTP/1.1"
+        // headline (response): "HTTP/1.1 200 OK"
+        std::string& headline = m_header_parser.headline();
+        size_t space1 = headline.find(" ");
+        if (space1 == std::string::npos) {
+            return "";
+        }
+        size_t space2 = headline.find(" ", space1);
+        if (space2 == std::string::npos) {
+            return "";
+        }
+        return headline.substr(space1, space2);
+    }
+
+    std::string _headline_third() {
+        // headline (request): "GET / HTTP/1.1"
+        // headline (response): "HTTP/1.1 200 OK"
+        std::string& headline = m_header_parser.headline();
+        size_t space1 = headline.find(" ");
+        if (space1 == std::string::npos) {
+            return "";
+        }
+        size_t space2 = headline.find(" ", space1);
+        if (space2 == std::string::npos) {
+            return "";
+        }
+        return headline.substr(space2);
+    }
+
+    std::string& body() {
+        return m_header_parser.extra_body();
+    }
+
+    size_t _extract_content_length() {
+        string_map& headers = m_header_parser.headers();
+        auto it = headers.find("content-length");
+        if (it == headers.end()) { // not found
+            return 0;
+        }
+        try {
+            return std::stoi(it->second);
+        } catch (std::invalid_argument& e) {
+            return 0;
+        }
     }
 
     void push_chunk(std::string_view chunk) {
         if (!m_header_parser.header_finished()) {
             m_header_parser.push_chunk(chunk);
             if (m_header_parser.header_finished()) {
-                m_content_length
+                m_content_length = _extract_content_length();
             }
             
         }
+    }
+};
+
+template <typename HeaderParser = http11_header_parser>
+struct http_request_parser: public _http_parser_base<HeaderParser> {
+    std::string method() {
+        return this->_headline_first();
+    }
+
+    std::string url() {
+        return this->_headline_second();
+    }
+
+    std::string http_version() {
+        return this->_headline_third();
+    }
+};
+
+template <typename HeaderParser = http11_header_parser>
+struct http_response_parser: public _http_parser_base<HeaderParser> {
+    std::string http_version() {
+        return this->_headline_first();
+    }
+
+    int status() {
+        std::string s = this->_headline_second();
+        try {
+            return std::stoi(s);
+        } catch (std::logic_error& e) { // std::out_of_range and std::invalid_argument
+            return -1;
+        }
+    }
+
+    std::string status_string() {
+        return this->_headline_second();
     }
 };
 
@@ -193,13 +338,13 @@ int main() {
         int connid = CHECK_CALL(accept, listenfd, &addr.m_addr, &addr.m_addrlen);
         pool.emplace_back([connid] {
             char buf[1024];
-            http_request_parser req_parse;
+            _http_parser_base req_parse;
             do {
                 size_t n = CHECK_CALL(read, connid, buf, sizeof(buf));
                 req_parse.push_chunk(std::string_view(buf, n));
-            } while (req_parse.need_more_chunks());
-            std::string header = req_parse.m_header;
-            std::string body = req_parse.m_body;
+            } while (req_parse.request_finished());
+            std::string header = req_parse.headers_raw();
+            std::string body = req_parse.body();
             fmt::print("Request header: {}\n", header);
             fmt::print("Request body: {}\n", body);
             std::string res = fmt::format("HTTP/1.1 200 OK\r\nServer: co_http\r\nConnection: close\r\nContent-length: {}\r\n\r\n{}", body.size(), body);
