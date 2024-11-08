80a318d55e89b9391a4d392786b2daad963a3557
lantel-wm <zyzh@smail.nju.edu.cn>
2024-11-09 00:51:38 +0800

    epoll read

--- src/server.cpp
+++ src/server.cpp
@@ -12,15 +12,15 @@
 #include <cwchar>
 #include <fcntl.h>
 #include <fmt/core.h>
-#include <queue>
 #include <string>
 #include <string_view>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <utility>
 
-std::queue<callback<>> to_be_called_later;
+int epollfd;
 
 struct async_file {
   int m_fd;
@@ -29,6 +29,11 @@ struct async_file {
     int flags = CHECK_CALL(fcntl, fd, F_GETFL);
     flags |= O_NONBLOCK; // set file to non-block
     CHECK_CALL(fcntl, fd, F_SETFL, flags);
+
+    struct epoll_event event;
+    event.events = EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+
     return async_file{fd};
   }
 
@@ -45,11 +50,17 @@ struct async_file {
     ret = CHECK_CALL_EXCEPT(EAGAIN, read, m_fd, buf.data(), buf.size());
     if (ret != -1) { // EAGAIN
       cb(ret);
-    } else {
-      to_be_called_later.push([this, buf, cb = std::move(cb)]() mutable {
-        async_read(buf, std::move(cb));
-      });
+      return;
     }
+
+    callback<> resume = [this, buf, cb = std::move(cb)]() mutable {
+      async_read(buf, std::move(cb));
+    };
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLET;
+    event.data.ptr = resume.leak_address();
+    epoll_ctl(epollfd, EPOLL_CTL_MOD, m_fd, &event);
   }
 
   ssize_t sync_write(bytes_view buf) {
@@ -58,6 +69,21 @@ struct async_file {
 
   size_t sync_write(std::string_view buf) {
     return CHECK_CALL_EXCEPT(EPIPE, write, m_fd, buf.data(), buf.size());
+  }
+
+  int sync_accept(struct sockaddr *addr, socklen_t *addrlen) {
+    int connid = CHECK_CALL(accept, m_fd, addr, addrlen);
+    fmt::print("Accept a conncetion: {}\n", connid);
+    return connid;
+  }
+
+  void async_accept(struct sockaddr *addr, socklen_t *addrlen, callback<> cb) {}
+
+  // void async_accept
+
+  void close_file() {
+    epoll_ctl(epollfd, EPOLL_CTL_DEL, m_fd, nullptr);
+    close(m_fd);
   }
 };
 
@@ -117,30 +143,47 @@ struct http_connection_handler {
   }
 
   void do_close() {
-    close(m_conn.m_fd);
+    m_conn.close_file();
     delete this; // the other way of managing lifetime is shared_ptr
   }
 };
 
-void server() {
-  std::string port = "8081";
-  fmt::print("Listening 127.0.0.1:{}\n", port);
-  address_resolver resolver;
-  resolver.resolve("127.0.0.1", port);
-  auto info = resolver.get_first_entry();
-  int listenfd = info.create_socket_and_bind();
-  CHECK_CALL(listen, listenfd, SOMAXCONN);
+struct http_connection_accepter {
+  async_file m_listen;
+  address_resolver::address m_addr;
 
-  address_resolver::address addr;
-  int connid = CHECK_CALL(accept, listenfd, &addr.m_addr, &addr.m_addrlen);
-  fmt::print("Accept a conncetion: {}\n", connid);
+  void do_init() {
+    std::string port = "8080";
+    fmt::print("Listening 127.0.0.1:{}\n", port);
+    address_resolver resolver;
+    resolver.resolve("127.0.0.1", port);
+    auto entry = resolver.get_first_entry();
+    m_listen = async_file::async_warp(entry.create_socket_and_bind());
+    m_listen.async_accept(&m_addr.m_addr, &m_addr.m_addrlen, [](int connfd) {
+      fmt::print("Connection accepted: {}\n", connfd);
+      http_connection_handler *conn_handler = new http_connection_handler{};
+    });
+  }
+};
+
+void server() {
+
+  epollfd = epoll_create1(0);
   // allocate conn_handler on heap, make it live out of this scope
   auto conn_handler = new http_connection_handler{};
   conn_handler->do_init(connid);
-  while (!to_be_called_later.empty()) {
-    auto task = std::move(to_be_called_later.front());
-    to_be_called_later.pop();
-    task();
+
+  struct epoll_event events[10];
+
+  while (true) {
+    int ret = epoll_wait(epollfd, events, 10, -1);
+    if (ret < 0) {
+      throw;
+    }
+    for (int i = 0; i < ret; i++) {
+      auto cb = callback<>::from_address(events[i].data.ptr);
+      cb();
+    }
   }
   fmt::print("All tasks are done.\n");
 }
